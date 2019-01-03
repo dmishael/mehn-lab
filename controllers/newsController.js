c1f4bd2d63051fe5c88a37ba6d636f618271afb0
joe kim <j80n89@gmail.com>
2019-01-03 15:21:58 -0500

    joe unhandled promise

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -1,11 +1,11 @@
-
+const Newslink = require('../models/Newslink');
 
 const newsController = {
     // console.log('path "/" accessed')
     index: (req, res) => {
         Newslink.find({}).then(newslinks => {
             console.log(newslinks)
-            res.render('/', { newslink: newslink })
+            res.render('/', { newslinks: newslinks })
         })
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const newsController = require('./controllers/newsController')
+const newsController = require('../controllers/newsController')
 //require controller
 
+router.get('/', newsController.index)
 
 
 module.exports = router
