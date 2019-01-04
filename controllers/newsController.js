29c2219f5cd85253947f378ce73a5888d6150cfe
joe kim <j80n89@gmail.com>
2019-01-03 23:22:14 -0500

    Joe Daniel struggling to make the button work

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -17,7 +17,24 @@ const newsController = {
         console.log('why')
     },
     create: (req, res) => {
-        console.log('yes')
+        console.log(req.query.title)
+        console.log(req.query.urlLink)
+        console.log(req.query.description)
+        console.log(req.query.createdAt)
+
+        Newslink.create({
+            urlLink: req.body.urlLink,
+            title: req.body.title,
+            description: req.body.description,
+            createdAt: {
+                type: Date.now()
+            }
+
+        }).then(newslink => {
+            // Now that we've created the user, let's display it
+            // by rendering the user show action for our new user:
+            res.redirect(`/Newslink/${newslink._id}`);
+        });
     }
 }
 
